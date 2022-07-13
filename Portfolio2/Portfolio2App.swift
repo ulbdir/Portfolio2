8ef3b89359b8f7941927560d32151ccdb7fcece2
Dirk Ulbricht <ulbdir@gmx.de>
2022-07-13 20:33:18 +0200

    Initial Commit

---
+++ Portfolio2/Portfolio2App.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio2App.swift
+//  Portfolio2
+//
+//  Created by Dirk Ulbricht on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
